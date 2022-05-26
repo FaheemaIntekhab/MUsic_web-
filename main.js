584adc72b711e62a9f1424e110b1df98e2868cab
FaheemaIntekhab <89588634+FaheemaIntekhab@users.noreply.github.com>
2022-05-26 20:33:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+var whistle = "";
+var on_the_ground = "";
+
+function preload()
+{
+    whistle = loadSound("whistle.mp3");
+    on_the_ground = loadSound("on the ground.mp3")
+}
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+
+function draw()
+{
+    image(video,0,0,600,500);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rigthWristY = " + rightWristY);
+    }
+}
